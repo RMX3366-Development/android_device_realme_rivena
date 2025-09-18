459ee426e0020745d5fea72b6e35023118486294
LuK1337 <priv.luk@gmail.com>
2025-09-18 17:39:32 +0800

    rivena: Add blob_fixup_nop_call for extract script
    Change-Id: I5bca1b1df4a3e6f74c9a1824d3279613c34df523

--- extract-files.py
+++ extract-files.py
@@ -14,12 +14,54 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
     'hardware/oplus',
     'vendor/realme/sm8250-common',
     'vendor/qcom/opensource/display',
 ]
+
+def blob_fixup_nop_call(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    call_instruction: str,
+    disassemble_symbol: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            f'--disassemble-symbols={disassemble_symbol}',
+            file_path,
+        ]
+    ).splitlines():
+        line = line.split(maxsplit=3)
+
+        if len(line) != 4:
+            continue
+
+        offset, _, instruction, args = line
+
+        if instruction != call_instruction:
+            continue
+
+        if not args.endswith(f' <{symbol}>'):
+            continue
+
+        with open(file_path, 'rb+') as f:
+            f.seek(int(offset[:-1], 16))
+            f.write(b'\x1f\x20\x03\xd5')  # AArch64 NOP
+
+        break
 
 blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
