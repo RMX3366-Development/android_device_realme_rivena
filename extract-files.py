bcd54102c8a2539b945ca0967c4a51d0842fb0da
ermyltsor <lamiimuk@163.com>
2025-10-12 20:57:16 +0800

    rivena: Drop Hotword enrollment It seems to exist only in non mainland China firmware. Of course, we don't care either.
    Change-Id: If706487df1dbdf100a148427f80a14305910cf00

--- extract-files.py
+++ extract-files.py
@@ -101,8 +101,6 @@ blob_fixups: blob_fixups_user_type = {
         .call(blob_fixup_nop_call, 'bl', '_ZN4pxlw18qcomSetDisplayModeEPvii', '_ZN4pxlw15irisSetConfigExEiiPKi@plt'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
         .apktool_patch('blob-patches/PowerOffAlarm.patch'),
-    'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
-        .regex_replace('/my_product', '/product'),
     'system_ext/bin/wfdservice': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
