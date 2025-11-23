0c67872753b3a6e9114f1912a75c3255bc247e25
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    rivena: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,8 @@ blob_fixups: blob_fixups_user_type = {
         .call(blob_fixup_nop_call, 'bl', '_ZN4pxlw18qcomSetDisplayModeEPvii', '_ZN4pxlw15irisSetConfigExEiiPKi@plt'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
         .apktool_patch('blob-patches/PowerOffAlarm.patch'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
