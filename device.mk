d05947e1e64e183a868c15a314a8b4fc128e4780
ermyltsor <lamiimuk@163.com>
2026-01-21 01:26:03 +0800

    rivena: Import oplus camera configuration for modify
    Change-Id: Ia9cb3b646dcd77d3e1eacf09036ac981e0231329

--- device.mk
+++ device.mk
@@ -94,6 +94,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/oplus_camera_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_config
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
